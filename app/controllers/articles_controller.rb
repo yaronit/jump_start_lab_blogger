2db78cb15268417d68d6c7f0970bdcc0421976f4
Yaron Itzhaky <yaronitzhaky@Yarons-Mac-mini.local>
2014-11-14 20:53:10 +0200

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+	include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
@@ -9,6 +9,30 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
-		
+		@article = Article.new
+	end
+	def create
+		@article = Article.new with_article_params
+		@article.save
+		flash.notice = "Article '#{@article.title}'' Created!"
+
+		redirect_to article_path @article
+	end
+	def edit
+		@article = Article.find params[:id]
+	end
+	def update
+		@article = Article.find params[:id]
+		@article.update with_article_params
+
+		flash.notice = "Article '#{@article.title}'' Updated!"
+		redirect_to article_path(@article)
+	end
+	def destroy
+		@article = Article.find params[:id]
+		@article.destroy
+		flash.notice = "Article '#{@article.title}'' Deleted!"
+
+		redirect_to articles_path
 	end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,5 @@
 module ArticlesHelper
+	def with_article_params
+		params.require(:article).permit :title, :body
+	end
 end
